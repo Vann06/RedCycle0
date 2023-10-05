2b8cc583103cca05ba09e358fcfef20f3aca7799
Vann06 <127684976+Vann06@users.noreply.github.com>
2023-10-05 15:36:53 -0600

    Clases controlador y Driver

---
+++ src/Clases/ControlContenedor.java
@@ -0,0 +1,19 @@
+package Clases;
+/*
+ * POO 2 Semestre
+ * Facultad de Ingenieria
+ * Departamento de Ciencias de la Computacion
+ * Fecha inicio: 19/09/2023
+ * Fecha final: 19/09/2023
+ *
+ * @author Nadissa Lopez 23764
+ * @author Vianka Castro 23201
+ * @author Ricardo Godinez 23247
+ * 
+ * Esta clase permite llevar un control sobre la cantidad 
+ * de residuos que se generan en fechas específicas del año, 
+ * 
+ * */
+public class ControlContenedor {
+
+}
---
+++ src/Clases/Driver.java
@@ -0,0 +1,22 @@
+package Clases;
+/*
+ * POO 2 Semestre
+ * Facultad de Ingenieria
+ * Departamento de Ciencias de la Computacion
+ * Fecha inicio: 19/09/2023
+ * Fecha final: 19/09/2023
+ *
+ * @author Nadissa Lopez 23764
+ * @author Vianka Castro 23201
+ * @author Ricardo Godinez 23247
+ * 
+ * Clase controladora que conecta todos los modelos del
+ * programa, es crucial porque concentra la lógica primordial del sistema.
+ * 
+ * */
+public class Driver {
+
+	
+	
+	
+}
